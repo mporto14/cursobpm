d601c68b7c4dbc52089a42ea74d04fdf9680eac2
padrao <mporto@gmail.com>
2017-08-13 18:06:07 -0300

    Curso

---
+++ scripts.sql
@@ -0,0 +1,74 @@
+-- MySQL dump 10.13  Distrib 5.5.25, for Win64 (x86)
+--
+-- Host: localhost    Database: cursobpm
+-- ------------------------------------------------------
+-- Server version	5.5.25-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aprovador_lancamento`
+--
+
+DROP TABLE IF EXISTS `aprovador_lancamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aprovador_lancamento` (
+  `id_lancamento` int(11) DEFAULT NULL,
+  `aprovador` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aprovador_lancamento`
+--
+
+LOCK TABLES `aprovador_lancamento` WRITE;
+/*!40000 ALTER TABLE `aprovador_lancamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aprovador_lancamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lancamento`
+--
+
+DROP TABLE IF EXISTS `lancamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lancamento` (
+  `id` int(11) DEFAULT NULL,
+  `motivo` varchar(50) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `observacao` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lancamento`
+--
+
+LOCK TABLES `lancamento` WRITE;
+/*!40000 ALTER TABLE `lancamento` DISABLE KEYS */;
+INSERT INTO `lancamento` VALUES (1,'TAXI',30.00,'Visita ao cliente XPTO'),(2,'TREI',27000.00,'Treinamento BPM'),(3,'TAXI',22.00,'Taxi do treinamento para casa');
+/*!40000 ALTER TABLE `lancamento` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-13 18:05:41
